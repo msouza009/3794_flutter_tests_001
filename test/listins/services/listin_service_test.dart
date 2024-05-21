ff2706930ff723440f5000a61942ce3b725c0782
Ricarth Lima <ricarth.lima@gmail.com>
2024-05-21 15:36:25 -0300

    v4.3 - configurando stubs para dependencias

--- test/listins/services/listin_service_test.dart
+++ test/listins/services/listin_service_test.dart
@@ -1,4 +1,5 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:flutter_listin/listins/models/listin.dart";
 import "package:flutter_listin/listins/services/listin_service.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:mockito/annotations.dart";
@@ -16,9 +17,42 @@ void main() {
   group("getListins:", () {
     test("O método deve retornar uma lista de Listin", () {
       MockFirebaseFirestore mockFirestore = MockFirebaseFirestore();
+
+      MockQuerySnapshot<Map<String, dynamic>> mockSnapshot =
+          MockQuerySnapshot();
+
+      MockCollectionReference<Map<String, dynamic>> mockCollection =
+          MockCollectionReference();
+
+      MockQueryDocumentSnapshot<Map<String, dynamic>> mockDoc001 =
+          MockQueryDocumentSnapshot();
+
+      MockQueryDocumentSnapshot<Map<String, dynamic>> mockDoc002 =
+          MockQueryDocumentSnapshot();
+
       String uid = "MEU_UID";
 
-      //when(mockFirestore.collection(uid).get()).thenAnswer((_) async => );
+      Listin listin001 = Listin(
+        id: "ID001",
+        name: "Feira de Maio",
+        obs: "",
+        dateCreate: DateTime.now().subtract(const Duration(days: 32)),
+        dateUpdate: DateTime.now().subtract(const Duration(days: 32)),
+      );
+      Listin listin002 = Listin(
+        id: "ID002",
+        name: "Feira de Junho",
+        obs: "",
+        dateCreate: DateTime.now(),
+        dateUpdate: DateTime.now(),
+      );
+
+      when(mockDoc001.data()).thenReturn(listin001.toMap());
+      when(mockDoc002.data()).thenReturn(listin002.toMap());
+      when(mockSnapshot.docs).thenReturn([mockDoc001, mockDoc002]);
+      when(mockCollection.get()).thenAnswer((_) async => mockSnapshot);
+
+      when(mockFirestore.collection(uid)).thenReturn(mockCollection);
 
       ListinService listinService = ListinService(
         firestore: mockFirestore,
