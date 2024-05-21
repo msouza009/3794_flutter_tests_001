46b28b72db5dc30c4bf8aef0bdecf3e9ba20d49e
Ricarth Lima <ricarth.lima@gmail.com>
2024-05-21 14:51:58 -0300

    v3.2 - preparando código para mocks

--- test/listins/services/listin_service_test.dart
+++ test/listins/services/listin_service_test.dart
@@ -1,5 +1,21 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:flutter_listin/listins/services/listin_service.dart";
+import "package:flutter_test/flutter_test.dart";
 import "package:mockito/annotations.dart";
 
+import "listin_service_test.mocks.dart";
+
 @GenerateNiceMocks([MockSpec<FirebaseFirestore>()])
-void main() {}
+void main() {
+  group("getListins:", () {
+    test("O método deve retornar uma lista de Listin", () {
+      MockFirebaseFirestore mockFirestore = MockFirebaseFirestore();
+      String uid = "MEU_UID";
+
+      ListinService listinService = ListinService(
+        firestore: mockFirestore,
+        uid: uid,
+      );
+    });
+  });
+}
