a4e9a03a067642a9490322f3be68cfdce797025b
ricarthlima <rrsl@cin.ufpe.br>
2024-06-03 14:35:28 -0300

    v2.2 - Conhecendo outros matcher

--- test/calculate_total_price_test.dart
+++ test/calculate_total_price_test.dart
@@ -1,6 +1,7 @@
 import "package:flutter_listin/products/helpers/calculate_total_price.dart";
 import "package:flutter_listin/products/model/product.dart";
 import "package:flutter_test/flutter_test.dart";
+import 'dart:math';
 
 void main() {
   test("Uma lista de produtos vazia, retorna total zero.", () {
@@ -37,5 +38,28 @@ void main() {
             "Espera-se que a função leve em consideração não apenas no valor mas a quantidade do item.",
       );
     },
+  );
+
+  test(
+    "A função não pode retornar valores negativos",
+    () {
+      List<Product> listProducts = [
+        Product(
+          id: "ID001",
+          name: "Feijão",
+          obs: "",
+          category: "",
+          isKilograms: false,
+          isPurchased: true,
+          amount: Random().nextInt(10).toDouble(),
+          price: Random().nextDouble() * 10,
+        ),
+      ];
+
+      double result = calculateTotalPriceFromListProduct(listProducts);
+
+      expect(result, isNonNegative);
+    },
+    retry: 1000000,
   );
 }
